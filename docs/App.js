e1cb5c76a9adc9d5401213f329542135da43e6c7
Grayson Hicks <graysonhicks@gmail.com>
2017-10-25 09:56:59 -0400

    importing

--- docs/App.js
+++ docs/App.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { H1, H2, H3, H4, Button } from "hemlock";
 
-const App = () => <div>Hello, world.</div>;
+const App = () => (
+	<div>
+		<H1>Hello, world.</H1>
+		<H2>Hello, world.</H2>
+		<H3>Hello, world.</H3>
+		<H4>Hello, world.</H4>
+		<Button>Hello, world.</Button>
+	</div>
+);
 
 export default App;
